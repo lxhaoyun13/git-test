73647968e02ed3775179cf1c2dab1bc913463df2
lx <1459517190@qq.com>
2021-07-31 18:02:48 +0800

    pushtest commit

--- src/main/java/com/lx/git/GitTest.java
+++ src/main/java/com/lx/git/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("ceshi");
         System.out.println("master");
         System.out.println("hot-fix");
+        System.out.println("push test");
     }
 }
