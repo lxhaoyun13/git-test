8c475f8cb385804662c7f8cc0b945ae20d7779b1
lx <1459517190@qq.com>
2021-07-31 17:31:11 +0800

    hot-fix commit

--- src/main/java/com/lx/git/GitTest.java
+++ src/main/java/com/lx/git/GitTest.java
@@ -3,6 +3,6 @@ package com.lx.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("ceshi");
-        System.out.println("hello");
+        System.out.println("master");
     }
 }
